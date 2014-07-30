899fc0513677b3f2b7b850c9d6f385b53119dc9d
mpresley42 <mpresley@princeton.edu>
2014-07-30 11:33:47 -0700

    Created a new git repository with all of the mpi files for runs on NERSC.

---
+++ monte_carlo/mpi_monte_carlo_gen_y_mult_fq.py
@@ -0,0 +1,171 @@
+print "I exist"
+from mpi4py import MPI 
+import sys, os
+import aipy as a, numpy as n, pylab as p
+import capo as C
+import basic_amp_aa_grid_gauss as agg
+import useful_functions as uf
+import matplotlib as mpl
+print "imported everything"
+
+def haslam_extrap(hasdat=None,fqs=n.array([0.1,])):
+    """
+    returns an array with rows that are gsm maps at the frequencies 
+    specified in fqs. So # of rows = # of fqs; # of columns = # of pix
+    """
+    alf0=2.8; var = 0.1 # from table on page 4 of http://arxiv.org/pdf/1106.0007.pdf
+    if hasdat==None:
+        hasmap = a.map.Map(fromfits='/global/homes/m/mpresley/scripts/general_files/fits_files/haslam408_32.fits')
+        hasdat = hasmap.map.map 
+    alf = n.random.randn(hasdat.shape[0])*var
+    # fqdat = n.zeros(len(fqs),hasdat.shape[0])
+    # for ii,fq in enumerate(fqs):
+    #     fqdat[ii,:] = hasdat*(fq/0.408)**(alf-alf0) 
+    fq_grid,hasdat_grid = n.meshgrid(hasdat,fqs) # fq varies on columns, hasdat is in the rows
+    fqdat = hasdat_grid*(fq_grid/0.408)**(alf-alf0) 
+    return fqdat
+
+def generate_sky_model_y(baselines,beam_sig,gsm_map=None,gsm_data_file=None):
+    """
+    y is a vector of the visibilities at different baselines
+    """
+    if gsm_data_file!=None:
+        healmap = a.map.Map(fromfits=gsm_data_file)
+    elif gsm_map!=None:
+        healmap = a.map.Map()
+        healmap.set_map(gsm_map)
+    else:
+        return None
+
+    px_array = n.arange(healmap.npix()) # gets an array of healpix pixel indices
+    rx,ry,rz = n.array(healmap.px2crd(px_array,ncrd=3)) # finds the topocentric coords for each healpix pixel
+    phi,theta = n.array(healmap.px2crd(px_array,ncrd=2)) # phi,theta in math coords
+    true_sky = healmap.map.map
+    amp = uf.gaussian(beam_sig,n.zeros_like(theta),phi)
+    dOmega = 4*n.pi/px_array.shape[0]
+
+    visibilities = n.zeros(baselines.shape[0],dtype='complex')
+    for kk in range(baselines.shape[0]):
+        bx,by,bz = baselines[kk]
+        Vis = amp*true_sky*n.exp(2j*n.pi*(bx*rx+by*ry+bz*rz))*dOmega
+        visibilities[kk] = n.sum(Vis)
+    return visibilities
+
+def gen_y_many_fqs(baselines,beam_sig,fq_gsm_map):
+    ys = n.zeros((fq_gsm_map.shape[0],baselines.shape[0]),dtype='complex')
+    for ii in n.arange(fq_gsm_map.shape[0]):
+        ys[ii,:] = generate_sky_model_y(baselines,beam_sig,gsm_map=fq_gsm_map[ii,:])
+    return ys 
+
+# define mpi parameters
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank() #int associated with each processor. ranges from 0 to number of processors
+size=comm.Get_size()
+master = 0
+num_slaves = size-1
+print "defined mpi params"
+
+# define monte_carlo directory location
+#mc_loc = '/global/homes/m/mpresley/scripts/monte_carlo/matrices'
+mc_loc = '/Users/mpresley/soft/capo/mep/nersc_scripts/scripts/monte_carlo/matrices'
+
+# define parameters related to calculation 
+hasmap = a.map.Map(fromfits='/Users/mpresley/soft/gsm/haslam408_32.fits')#XXX '/global/homes/m/mpresley/scripts/general_files/fits_files/haslam408_32.fits')
+fqs = n.array([50.,60.,70.])*0.001 #XXX n.arange(50,91,2)*0.001
+print 'fqs ',fqs  
+
+_,num0,beam_sig,del_bl,num_bl = sys.argv
+num0=int(num0);beam_sig=float(beam_sig); del_bl=float(del_bl); num_bl=int(num_bl)
+baselines = agg.make_pos_array(del_bl,num_bl)
+
+save_interval = 5 #XXX 10000
+save_num = 0
+savekey = 'grid_del_bl_{0:.2f}_num_bl_{1}_beam_sig_{2:.2f}'.format(del_bl,num_bl,beam_sig)
+
+print "defined hasmap stuff"
+
+# define parameters related to task-mastering
+num1 = baselines.shape[0]
+numToDo = num0
+num_sent = 0 # this functions both as a record of how many assignments have 
+             # been sent and as a tag marking which matrix entry was calculated
+num_recv = 0
+
+
+print "defined task-mastering stuff"
+
+# Big running loop
+# If I am the master process
+if rank==master:
+    print "I am the master! Muahaha!"
+    for fq in fqs:
+        if '{0}_fq+{1}'.format(savekey,fq) not in os.listdir(mc_loc):
+            os.mkdir('{0}/{1}_fq_{2}'.format(mc_loc,savekey,fq))
+    # send out first round of assignments
+    for kk in range(num_slaves):
+        selectedi = num_sent
+        print "num_sent = ",num_sent
+        comm.send(selectedi,dest=kk+1)
+        print "i = ",selectedi," was sent to slave ",kk+1
+        num_sent +=1
+    print "Master sent out first round of assignments"
+    # listen for results and send out new assignments
+    for kk in range(numToDo):
+        source,entries = comm.recv(source=MPI.ANY_SOURCE)
+        selectedi = comm.recv(source=source)
+        # stick entry into matrix 
+        print 'entries ',entries.shape
+        if num_recv%save_interval==0:
+            matrix = entries # fqs x bl
+        else:
+            matrix = n.dstack((matrix,entries)) # fqs x bl x mc 
+        print 'Master just received element (i,) = ',selectedi,' from slave ',source
+        # if there are more things to do, send out another assignment
+        if num_sent<numToDo:
+            selectedi = num_sent
+            comm.send(selectedi,dest=source)
+            print "Master sent out i = ",selectedi,' to slave ',source
+            num_sent +=1
+        else:
+            # send a -1 to tell slave that task is complete
+            comm.send(-1,dest=source)
+            print "Master sent out the finished i to slave ",source
+        if num_recv%save_interval==save_interval-1:
+            for ii,y_mat in enumerate(n.vsplit(matrix,matrix.shape[0])):
+                print 'ymat ',y_mat.shape
+                n.savez_compressed('{0}/{1}_fq_{2}/mc_{3}'.format(mc_loc,savekey,fqs[ii],save_num),matrix=y_mat[0])
+            matrix=None
+            save_num+=1
+        num_recv += 1
+
+# If I am a slave and there are not more slaves than jobs
+elif rank<=numToDo:
+    print "I am slave ",rank
+    complete = False
+    while not complete:
+        # Get assignment
+        selectedi = comm.recv(source=master)
+        print "slave ",rank," just recieved i = ",selectedi
+        if selectedi==-1:
+            # if there are no more jobs
+            complete=True
+            print "slave ",rank," acknoledges job completion"
+        else:
+            # compute the matrix element
+            fq_gsm_map = haslam_extrap(hasdat=hasmap.map.map,fqs=fqs)
+            element = gen_y_many_fqs(baselines,beam_sig,fq_gsm_map)
+            # send answer back
+            comm.send((rank,element),dest=master)
+            comm.send(selectedi,dest=master)
+            print "Slave ",rank," sent back i = ",selectedi
+comm.Barrier()
+
+# if rank==master:
+#     print "The master will now save the matrix."
+#     n.savez_compressed('/global/homes/m/mpresley/scripts/monte_carlo/matrices/monte_carlo_{0}_'.format(savekey),matrix=matrix)
+#     print matrix.shape
+
+MPI.Finalize()
+
+
+
---
+++ mpi_gsm_hybrid.py
@@ -0,0 +1,162 @@
+from mpi4py import MPI 
+import sys, os
+from time import time
+import numpy as n
+import useful_functions as uf
+import aipy as a
+import basic_amp_aa_grid_gauss as agg
+
+def compute_element(bli,blj,amp):
+    bix,biy,biz = bli; bjx,bjy,bjz = blj
+    element = 0
+#    print 'amp shape = ',amp.shape
+#    print 'px_array shape = ',px_array.shape
+    for kk in px_array:
+        rx,ry,rz = crd_array[:,kk]          
+        Gik = amp[kk]*n.exp(-2j*n.pi*fq*(bix*rx+biy*ry+biz*rz))*dOmega
+        Gjk_star = n.conj(amp[kk]*n.exp(-2j*n.pi*fq*(bjx*rx+bjy*ry+bjz*rz)))
+        Rkk = Rdata[kk]*Rdata[kk]
+        element += Gik*Rkk*Gjk_star
+    return element
+
+import_time = time()
+
+# define mpi parameters
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank() #int associated with each processor. ranges from 0 to number of processors
+size=comm.Get_size()
+master = 0
+num_slaves = size-1
+
+# define file location 
+scripts_loc = '/global/homes/m/mpresley/scripts'
+#scripts_loc = '/Users/mpresley/soft/capo/mep/nersc_scripts/scripts'
+
+# get system arguments
+_,fq,del_bl,num_bl = sys.argv
+fq=float(fq),del_bl=float(del_bl);num_bl=int(num_bl)
+
+# define parameters related to calculation
+healmap = a.map.Map(fromfits='{0}/general_files/fits_files/hi1001_32.fits'.format(scripts_loc))
+global px_array; px_array = n.arange(healmap.npix()) # gets an array of healpix pixel indices
+global crd_array; crd_array = n.array(healmap.px2crd(px_array,ncrd=3)) # finds the topocentric coords for each healpix pixel
+global Rdata; Rdata = healmap.map.map
+global dOmega; dOmega = 4*n.pi/px_array.shape[0]
+phi,theta = n.array(healmap.px2crd(px_array,ncrd=2))
+#print 'theta max = ',max(theta)
+#print 'phi max = ',max(phi)
+
+beamsig_largebm = 10/(2*n.pi*del_bl*(num_bl-1)) #1.0                                         
+beamsig_smallbm = 10/(2*n.pi*del_bl) #0.25          
+smallbm_inds = (int(n.floor(num_bl/2)),int(n.floor(num_bl/2)))
+
+savekey = 'hybrid_fq_{0:.2f}_del_bl_{1:.2f}_num_bl_{2}'.format(fq,del_bl,num_bl)
+
+amp_largebm = uf.gaussian(beamsig_largebm,n.zeros_like(theta),phi)
+amp_smallbm = uf.gaussian(beamsig_smallbm,n.zeros_like(theta),phi)
+baselines = agg.make_pos_array(del_bl,num_bl)
+
+# define matrix to be calculated
+num = len(baselines)
+matrix = n.zeros([num,num],dtype=n.complex)
+# define parameters related to task-mastering
+numToDo = 50 #XXX num*(num+1)/2
+print 'numToDo = ',numToDo
+assn_inds = []
+for ii in range(num+1):
+    for jj in range(ii+1):
+        assn_inds.append((ii,jj))
+num_sent = 0 # this functions both as a record of how many assignments have 
+             # been sent and as a tag marking which matrix entry was calculated
+num_complete = 0
+
+# Big running loop
+# If I am the master process
+if rank==master:
+    print "I am the master! Muahaha!"
+    print "import time: ",import_time
+    setup_time = time()
+    print "setup time: ",setup_time
+    # define stuff related to saving
+    save_interval = 20#XXX 1000 
+    save_num = 0; num_recv = 0
+
+    if savekey not in os.listdir('{0}/gsm_matrices/'.format(scripts_loc)):
+        os.mkdir('{0}/gsm_matrices/{1}'.format(scripts_loc,savekey))
+    # send out first round of assignments
+    for kk in range(num_slaves):
+        selectedi, selectedj = assn_inds[kk]
+        print "num_sent = ",num_sent
+        comm.send(selectedi,dest=kk+1)
+        comm.send(selectedj,dest=kk+1)
+        print "i,j = ",selectedi,selectedj," was sent to slave ",kk+1
+        num_sent +=1
+    print "Master sent out first round of assignments"
+    # listen for results and send out new assignments
+    for kk in range(numToDo):
+        source,entry = comm.recv(source=MPI.ANY_SOURCE)
+        selectedi = comm.recv(source=source)
+        selectedj = comm.recv(source=source)
+        # stick entry into matrix 
+        matrix[selectedi,selectedj] = entry
+        matrix[selectedj,selectedi] = n.conj(entry)
+        print 'Master just received element (i,j) = ',selectedi,selectedj,' from slave ',source
+        num_complete += 1
+        print 'num complete = ',num_complete
+        # if there are more things to do, send out another assignment
+        if num_sent<numToDo:
+            selectedi, selectedj = assn_inds[kk]
+            comm.send(selectedi,dest=source)
+            comm.send(selectedj,dest=source)
+            print "Master sent out i,j = ",selectedi, selectedj,' to slave ',source
+            num_sent +=1
+        else:
+            # send a -1 to tell slave that task is complete
+            comm.send(-1,dest=source)
+            comm.send(-1,dest=source)
+            print "Master sent out the finished i,j to slave ",source
+        if num_recv%save_interval==save_interval-1 or num_recv==numToDo:
+            n.savez_compressed('{0}/gsm_matrices/{1}/gsm_hy_{2}'.format(scripts_loc,savekey,save_num),matrix=matrix)
+            save_time = time()
+            print "save {0} time: {1}".format(save_num,save_time)
+            print "time to do {0} runs = {1}".format((save_num+1)*save_interval,save_time-setup_time)
+            #matrix=None
+            save_num+=1
+        num_recv += 1
+# If I am a slave and there are not more slaves than jobs
+elif rank<=numToDo:
+    print "I am slave ",rank
+    complete = False
+    while not complete:
+        # Get assignment
+        selectedi = comm.recv(source=master)
+        selectedj = comm.recv(source=master)
+        print "slave ",rank," just recieved i,j = ",selectedi,selectedj
+        if selectedi==-1:
+            # if there are no more jobs
+            complete=True
+            print "slave ",rank," acknoledges job completion"
+        else:
+            # compute the matrix element
+            if (selectedi,selectedj)==smallbm_inds: amp = amp_smallbm # if it's the baseline with the large smear/small beam                                                               
+            else: amp = amp_largebm
+            bli = baselines[selectedi,:]
+            blj = baselines[selectedj,:]
+            element = compute_element(bli,blj,amp)
+            # send answer back
+            comm.send((rank,element),dest=master)
+            comm.send(selectedi,dest=master)
+            comm.send(selectedj,dest=master)
+            print "Slave ",rank," sent back i,j = ",selectedi,selectedj
+comm.Barrier()
+
+if rank==master:
+    print "The master will now save the matrix."
+    n.savez_compressed('{0}/gsm_matrices/{1}/{1}'.format(scripts_loc,savekey),matrix=matrix,baselines=baselines)
+    done_time = time()
+    print "done time: ",done_time
+    print "total run time: ",done_time - import_time
+    print "run time w/o startup: ",done_time - setup_time
+
+MPI.Finalize()
+
