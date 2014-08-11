6005f7caf612c05ef39afbff277e2444872c21cc
Morgan Presley <mpresley@princeton.edu>
2014-08-11 10:55:49 -0700

    modified python script to submit mult fq Q matrix jobs.

--- run_files/make_many_sub_files_grid.py
+++ run_files/make_many_sub_files_grid.py
@@ -5,8 +5,8 @@ kk=0
 for beam_sig in (0.087,0.175,0.349,0.689,1.047):
     for del_bl in (4,6,8):#,10,20):#4,6)
         fcontent = """#PBS -q regular
-#PBS -l mppwidth=72
-#PBS -l walltime=05:00:00
+#PBS -l mppwidth=48
+#PBS -l walltime=03:00:00
 #PBS -N Q_grid_mf_{0}
 #PBS -e out_files/Q_grid_mf_{1}.$PBS_JOBID.err
 #PBS -o out_files/Q_grid_mf_{2}.$PBS_JOBID.out
@@ -17,7 +17,7 @@ module load mpi4py
 
 cd $PBS_O_WORKDIR 
 # beam_sig,del_bl,num_bl (for one side of grid)
-aprun -n 72 python-mpi /global/homes/m/mpresley/gs_mpi/mpi_Q_matrix_grid_mult_fq_2.py {3} {4} 10
+aprun -n 48 python-mpi /global/homes/m/mpresley/gs_mpi/mpi_Q_matrix_grid_mult_fq_2.py {3} {4} 10
 """.format(kk,kk,kk,beam_sig,del_bl)
         with open('./run_mpi_Q_matrix_grid_{0}.sh'.format(kk), 'w') as file:
             file.writelines(fcontent)
