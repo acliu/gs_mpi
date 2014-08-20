407caa5184f3bc4dce3c143b353fefb8594c3b00
Adrian Liu <acliu@berkeley.edu>
2014-08-20 16:46:27 -0700

    New carver script for generating Q matrices

--- scripts/mpi_Q_matrix_grid_mult_fq_3_carver.sh
+++ scripts/mpi_Q_matrix_grid_mult_fq_3_carver.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 #PBS -S /bin/bash
-#PBS -N mpi_gsm_grid_mult_fq_3_carver
+#PBS -N mpi_Q_matrix_grid_mult_fq_3_carver
 #PBS -j eo
 #PBS -l nodes=3:ppn=8,walltime=00:30:00
 #PBS -q debug
@@ -9,7 +9,6 @@
 
 codeLoc="/global/scratch2/sd/acliu/GlobalSignalInterferometer/gs_mpi"
 outputLoc="/global/scratch2/sd/acliu/GlobalSignalInterferometer/gs_data"
-templateMap='/global/scratch2/sd/acliu/GlobalSignalInterferometer/gs_mpi/skyTemplates/GSM_templateMap_70MHz_nside32.fits'
 numProcs=24
 maxl=10
 beam_sig=0.064614317 # in radians.  HERA beam at 150 MHz
@@ -18,6 +17,7 @@ sqGridSideLen=10
 lowerFreq=50 # in MHz
 upperFreq=90 # in MHz
 deltaFreq=2 # in MHz
+
 
 module load python/2.7.3
 module load numpy
@@ -28,6 +28,5 @@ module load mpi4py
 # For running on my laptop or Carver
 echo "Starting run now..."
 date
-mpirun -np $numProcs python-mpi "$codeLoc/mpi_gsm_grid_mult_fq_3.py" $templateMap $outputLoc $lowerFreq $upperFreq $deltaFreq $maxl $beam_sig $del_bl $sqGridSideLen
+mpirun -np $numProcs python-mpi "$codeLoc/mpi_Q_matrix_grid_mult_fq_3.py" $outputLoc $maxl $beam_sig $del_bl $sqGridSideLen $lowerFreq $upperFreq $deltaFreq
 date
-
