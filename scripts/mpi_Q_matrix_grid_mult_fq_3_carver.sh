c3107f90f5a46cdf3f461dd354adcd4a44ac6db4
Adrian Liu <acliu@berkeley.edu>
2014-08-20 18:01:54 -0700

    Change in Q matrix submission script

--- scripts/mpi_Q_matrix_grid_mult_fq_3_carver.sh
+++ scripts/mpi_Q_matrix_grid_mult_fq_3_carver.sh
@@ -3,17 +3,17 @@
 #PBS -S /bin/bash
 #PBS -N mpi_Q_matrix_grid_mult_fq_3_carver
 #PBS -j eo
-#PBS -l nodes=3:ppn=8,walltime=00:30:00
+#PBS -l nodes=9:ppn=8,walltime=04:00:00
 #PBS -q debug
 #PBS -A m1871
 
 codeLoc="/global/scratch2/sd/acliu/GlobalSignalInterferometer/gs_mpi"
 outputLoc="/global/scratch2/sd/acliu/GlobalSignalInterferometer/gs_data"
-numProcs=24
-maxl=10
+numProcs=48
+maxl=30
 beam_sig=0.064614317 # in radians.  HERA beam at 150 MHz
 del_bl=23 # in nanoseconds.  HERA
-sqGridSideLen=10
+sqGridSideLen=5
 lowerFreq=50 # in MHz
 upperFreq=90 # in MHz
 deltaFreq=2 # in MHz
