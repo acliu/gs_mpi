7c8c6d37445c0c26ae77de6a05ea939886d8a748
Morgan Presley <mpresley@princeton.edu>
2014-08-21 09:43:53 -0700

    Changed make_many_sub_files_grid.py from debug to regular

--- run_files/make_many_sub_files_grid.py
+++ run_files/make_many_sub_files_grid.py
@@ -4,9 +4,9 @@ import os
 kk=0
 for beam_sig in (0.087,):#0.175,0.349,0.689,1.047):
     for del_bl in (4,):#6,8):
-        fcontent = """#PBS -q debug
-#PBS -l mppwidth=24
-#PBS -l walltime=00:30:00
+        fcontent = """#PBS -q regular
+#PBS -l mppwidth=48
+#PBS -l walltime=02:00:00
 #PBS -N Q_grid_mf_{0}
 #PBS -e out_files/Q_grid_mf_{1}.$PBS_JOBID.err
 #PBS -o out_files/Q_grid_mf_{2}.$PBS_JOBID.out
@@ -17,7 +17,7 @@ module load mpi4py
 
 cd $PBS_O_WORKDIR 
 # beam_sig,del_bl,num_bl (for one side of grid)
-aprun -n 24 python-mpi /global/homes/m/mpresley/gs_mpi/mpi_Q_matrix_grid_mult_fq_2.py {3} {4} 10
+aprun -n 48 python-mpi /global/homes/m/mpresley/gs_mpi/mpi_Q_matrix_grid_mult_fq_2.py {3} {4} 10
 """.format(kk,kk,kk,beam_sig,del_bl)
         with open('./run_mpi_Q_matrix_grid_{0}.sh'.format(kk), 'w') as file:
             file.writelines(fcontent)
@@ -28,9 +28,9 @@ kk=0
 for beam_sig in (0.087,):#0.175,0.349,0.689,1.047):
     for del_bl in (4,):#6,8):#10,20)
 #for beam_sig, del_bl in ((0.69,20),(1.05,20),(0.69,10),(1.05,10),(0.69,8),(1.05,8),(0.09,6),(0.09,4)):
-        fcontent = """#PBS -q debug
-#PBS -l mppwidth=24
-#PBS -l walltime=00:30:00
+        fcontent = """#PBS -q regular
+#PBS -l mppwidth=72
+#PBS -l walltime=04:00:00
 #PBS -N gsm_grid_{0}
 #PBS -e out_files/gsm_grid_{1}.$PBS_JOBID.err
 #PBS -o out_files/gsm_grid_{2}.$PBS_JOBID.out
@@ -41,7 +41,7 @@ module load mpi4py
 
 cd $PBS_O_WORKDIR 
 # beam_sig,del_bl,num_bl (for one side of grid)
-aprun -n 24 python-mpi /global/homes/m/mpresley/scripts/mpi_gsm_grid.py {3} {4} 10
+aprun -n 72 python-mpi /global/homes/m/mpresley/scripts/mpi_gsm_grid.py {3} {4} 10
 """.format(kk,kk,kk,beam_sig,del_bl)
         with open('./run_mpi_gsm_grid_{0}.sh'.format(kk), 'w') as file:
             file.writelines(fcontent)
