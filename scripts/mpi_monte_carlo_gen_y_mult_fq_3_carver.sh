11f666df1137109b20d6ba45cfe0bb6f4513c165
Adrian Liu <acliu@berkeley.edu>
2014-09-02 18:56:18 -0700

    Going back to old script...

--- scripts/mpi_monte_carlo_gen_y_mult_fq_3_carver.sh
+++ scripts/mpi_monte_carlo_gen_y_mult_fq_3_carver.sh
@@ -3,7 +3,7 @@
 #PBS -S /bin/bash
 #PBS -N mpi_monte_carlo_gen_y_fq_3_carver
 #PBS -j eo
-#PBS -l nodes=3:ppn=8,walltime=00:30:00
+#PBS -l nodes=3:ppn=4,walltime=00:30:00,pvmem=5GB
 #PBS -q regular
 #PBS -A m1871
 
@@ -35,7 +35,7 @@ variableBeam=0
 
 numMCs=10000
 saveInterval=150
-numProcs=24
+numProcs=12
 
 module load python/2.7.3
 module load numpy
